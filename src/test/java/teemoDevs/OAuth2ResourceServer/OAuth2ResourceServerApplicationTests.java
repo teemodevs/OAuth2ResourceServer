0f90ec3ba9115ef2301f701630691626ea5951d9
yechanpark <qkrdpcks0419@naver.com>
2019-04-13 20:21:24 +0900

    First Commit

---
+++ src/test/java/teemoDevs/OAuth2ResourceServer/OAuth2ResourceServerApplicationTests.java
@@ -0,0 +1,16 @@
+package teemoDevs.OAuth2ResourceServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OAuth2ResourceServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
