704383fdcd8ab6d4e3a4f1f5031ac024d35605e4
yechanpark <qkrdpcks0419@naver.com>
2019-05-04 02:02:22 +0900

    OAuth2AuthorizationServer 책임분리, OAuth2ResourceServer 구현중

--- src/main/java/teemoDevs/OAuth2ResourceServer/OAuth2ResourceServerApplication.java
+++ src/main/java/teemoDevs/OAuth2ResourceServer/OAuth2ResourceServerApplication.java
@@ -2,7 +2,9 @@ package teemoDevs.OAuth2ResourceServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer // 자원서버 설정
 @SpringBootApplication
 public class OAuth2ResourceServerApplication {
 
